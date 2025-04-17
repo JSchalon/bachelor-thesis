9fae3ea50b7fda4103532c2c698adcdf333990ed
Junipeeer <181602566+Junipeeer@users.noreply.github.com>
2025-04-17 23:35:08 +0200

    reset dependencies

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/interaktionseditor/'  // Replace with your repo name
+    ? '/bachelor-thesis/' 
     : '/',
   configureWebpack: {
     module: {
